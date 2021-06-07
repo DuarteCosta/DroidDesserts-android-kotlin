7809539a81e05e17dcb2d40aa8a5bbebf87258ae
Duarte Costa <duartecosta3558@hotmail.com>
2021-06-07 18:08:06 +0100

    Second page

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,9 +9,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+     var message: String = "Select Order"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        floats.isEnabled = false;
         floats.setOnClickListener { view ->
             buyView(view)
 
@@ -20,23 +25,32 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun display (message: String) =
+    private fun display (message: String) =
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
     fun showDonut(view: View){
-        display(getString(R.string.donut_order_message))
+        message = getString(R.string.donut_order_message)
+        display(message)
+        floats.isEnabled = true;
     }
 
     fun showIce(view: View){
-        display(getString(R.string.ice_cream_order_message))
+        message = getString(R.string.ice_cream_order_message)
+        display(message)
+
+        floats.isEnabled = true;
     }
 
     fun showFroyo(view: View){
-        display(getString(R.string.froyo_order_message))
+        message = getString(R.string.froyo_order_message)
+        display(message)
+        floats.isEnabled = true;
+
     }
 
-    fun buyView(view: View){
+    private fun buyView(view: View){
         val intent = Intent(this, OrderActivity::class.java)
+        intent.putExtra("messag",message)
         startActivity(intent)
     }
 
--- app/src/main/java/com/example/myapplication/OrderActivity.kt
+++ app/src/main/java/com/example/myapplication/OrderActivity.kt
@@ -1,11 +1,45 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_order.*
+
 
 class OrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+        // Get the intent and its data.
+        val intent = intent.extras
+        var messag = intent?.getString("messag")
+        orderss.text = messag
+
+
+    }
+
+    private fun display(message: String) =
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+    fun onRadioButtonClicked(view: View) {
+        // Is the button now checked?
+        // Is the button now checked?
+        val checked = (view as RadioButton).isChecked
+        // Check which radio button was clicked.
+        when (view.getId()) {
+            same.id -> if (checked) // Same day service
+                display(getString(R.string.same_day_delivery))
+            next.id -> if (checked) // Next day delivery
+                display(getString(R.string.next_day_delivery))
+            pickUp.id -> if (checked) // Pick up
+                display(getString(R.string.pick_up))
+            else -> {
+            }
+        }
+
+
     }
 }
